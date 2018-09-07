3839566e5a52b168f2b63c0e35fe44cee47768a5
whj768702 <whj768702@gmail.com>
2018-09-07 08:49:38 +0800

    w3resource练习28.

---
+++ js/28.js
@@ -0,0 +1,7 @@
+function checkNumbers(x, y) {
+  return (x >= 50 && x <= 99) || (y >= 50 && y <= 99);
+}
+
+console.log(checkNumbers(12, 101));
+console.log(checkNumbers(52, 80));
+console.log(checkNumbers(15, 99));
