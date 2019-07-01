694d4322adaac882a30d9ae3b119ea7f5f13d6c6
whj768702 <whj768702@gmail.com>
2019-07-01 09:04:58 +0800

    proxy中get。

--- proxy/get.js
+++ proxy/get.js
@@ -1,16 +1,19 @@
 // The handler.get() method is a trap for getting a property value.
 const person = {
-  name: '我',
+  name: (id)=> {
+    return id;
+  },
   age: 28
 }
 const handler = {
-  get(target, prop) {
-    if (prop === 'age') {
-      return 18;
-    }
-    return target[prop];
+  get: (target, prop) => {
+    // if (prop === 'age') {
+      // return 18;
+    // }
+    // return target[prop];
+    return target['name'];
   }
 };
 const personProxy = new Proxy(person, handler);
 console.log(person.age); // 28
-console.log(personProxy.age); // 18
+console.log(personProxy.age(12312313)); // 18
