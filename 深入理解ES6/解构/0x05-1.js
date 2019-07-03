417ed3ebf83d8af8602434aeb99a0083faa9b280
whj768702 <whj768702@gmail.com>
2019-07-03 08:44:09 +0800

    对象解构。

--- 深入理解ES6/解构/0x05-1.js
+++ 深入理解ES6/解构/0x05-1.js
@@ -5,4 +5,13 @@ let node = {
 function outputInfo(value) {
   console.log(value);
 }
+// 函数参数解构
 outputInfo({type, name} = node);
+
+// 赋值给本地变量, 匹配不上的值为undefined
+let {type: localType, name: localName, value: localValue} = node;
+// 可以给默认值
+//let {type: localType, name: localName, value: localValue = 3} = node;
+console.log(localType);
+console.log(localName);
+console.log(localValue);
