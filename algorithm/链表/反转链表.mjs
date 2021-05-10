505e3399ceea056b09d470839242b724de583abb
whj768702 <whj768702@gmail.com>
2021-05-10 14:43:29 +0800

    feat(算法): 反转链表。

---
+++ algorithm/链表/反转链表.mjs
@@ -0,0 +1,18 @@
+import initList from "./生成链表.mjs";
+
+const listNode = initList(3);
+
+function reverse(head) {
+  if (!head.next) {
+    return head;
+  }
+  // 取最后一个节点
+  const last = reverse(head.next);
+  // 这块把A->B转换成B->A
+  head.next.next = head;
+  head.next = null;
+  return last;
+}
+
+const result = reverse(listNode);
+console.log(result);
