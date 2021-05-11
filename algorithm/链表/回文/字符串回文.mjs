a550cfedb04582381a4097b6b2aa4ca038cc490a
whj768702 <whj768702@gmail.com>
2021-05-11 09:06:26 +0800

    feat(算法): 判断字符串是否回文。

---
+++ algorithm/链表/回文/字符串回文.mjs
@@ -0,0 +1,18 @@
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] != str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
+console.log(isPalindrome('aaaabbbaaaa'));
+console.log(isPalindrome('aaaabbdbaaaa'));
+console.log(isPalindrome('aabbaaa'));
+console.log(isPalindrome('aaabbbaaaa'));
