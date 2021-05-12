c9e6c7543886a2227c6d0a7299e7e2e413502a48
whj768702 <whj768702@gmail.com>
2021-05-12 09:36:26 +0800

    feat(算法): 生成二叉树

---
+++ algorithm/二叉树/生成二叉树.mjs
@@ -0,0 +1,50 @@
+class TreeNode {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(key) {
+    const node = new TreeNode(key);
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      this.insertNode(this.root, node);
+    }
+  }
+  insertNode(root, node) {
+    if (node.data < root.data) {
+      if (root.left === null) {
+        root.left = node;
+      } else {
+        this.insertNode(root.left, node);
+      }
+    } else if (node.data > root.data) {
+      if (root.right === null) {
+        root.right = node;
+      } else {
+        this.insertNode(root.right, node);
+      }
+    }
+  }
+}
+
+function initBinaryTree(arr) {
+  let result = new BinaryTree();
+  // for (let i = length; i > 0; i--) {
+  // result.insert(i);
+  // }
+  arr.map(item => result.insert(item));
+
+  return result;
+}
+
+// const result = initBinaryTree([19, 8, 15, 24, 45, 12, 5]);
+// console.log(result);
+
+export { initBinaryTree };
