63d3d14e338b45cfc492548d0c019975d21b4b35
whj768702 <whj768702@gmail.com>
2022-03-12 16:43:28 +0800

    feat(极客时间webpack): 区分开发和生成环境配置，输出文件增加hash

---
+++ 极客时间/webpack/project/webpack.prod.js
@@ -0,0 +1,61 @@
+'use strict';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { webpack } = require('webpack');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]_[chunkhash:8].js',
+  },
+  mode: 'production', // production | development | none
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // Inject CSS into the DOM. 将CSS注入到DOM中。
+          'style-loader',
+          // The css-loader interprets @import and url() like import/require() and will resolve them.
+          //css-loader对@import和url()的解释就像import/require()一样，并会解决它们。
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]_[hash:8].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      publicPath: './',
+      template: 'index.html',
+    }),
+  ],
+  optimization: {
+    moduleIds: 'named',
+  },
+};
